1daef7c9ab5d8bfe0f898b60bc3a491a6ea826b9
Roman Kozin <romanko95.rk@gmail.com>
2021-06-04 02:58:55 +0300

    Fixes

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -43,6 +43,7 @@
 <script src="{{ asset('/assets/mdp/js/core/bootstrap-material-design.min.js') }}"></script>
 <script src="{{ asset('/assets/mdp/js/plugins/perfect-scrollbar.min.js') }}"></script>
 <script src="{{ asset('/assets/mdp/js/material-dashboard.js') }}"></script>
+<script src="{{ asset('/assets/mdp/js/dom.js') }}"></script>
 
 @stack('mdp::layout_js')
 

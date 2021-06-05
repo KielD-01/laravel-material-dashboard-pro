4795815217c1bc49a6f5a36462eed315741abf50
Roman Kozin <romanko95.rk@gmail.com>
2021-06-05 03:18:03 +0300

    - Updates coming

--- src/config/mdp/menu.php
+++ src/config/mdp/menu.php
@@ -7,13 +7,11 @@ use KielD01\LaravelMaterialDashboardPro\Helpers\MenuItem;
 use KielD01\LaravelMaterialDashboardPro\Helpers\MenuItemLinkType;
 
 return [
-	[
-		new MenuItem(
-			'Dashboard',
-			MenuItemLinkType::ROUTE,
-			'admin.dashboard',
-			new MaterialIcon('dashboard'),
-			[]
-		),
-	],
+	new MenuItem(
+		'Dashboard',
+		MenuItemLinkType::ROUTE,
+		'admin.dashboard',
+		new MaterialIcon('dashboard'),
+		[]
+	),
 ];
--- src/resources/views/elements/sidebar.blade.php
+++ src/resources/views/elements/sidebar.blade.php
@@ -42,60 +42,62 @@
             </div>
         </div>
         <ul class="nav">
-            <li class="nav-item active ">
-                <a class="nav-link" href="#">
-                    <i class="material-icons">dashboard</i>
-                    <p> Dashboard </p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" data-toggle="collapse" href="#pagesExamples">
-                    <i class="material-icons">image</i>
-                    <p> Pages
-                        <b class="caret"></b>
-                    </p>
-                </a>
-                <div class="collapse" id="pagesExamples">
-                    <ul class="nav">
-                        <li class="nav-item ">
-                            <a class="nav-link" href="#">
-                                <span class="sidebar-mini"> P </span>
-                                <span class="sidebar-normal"> Pricing </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" data-toggle="collapse" href="#componentsExamples">
-                    <i class="material-icons">apps</i>
-                    <p> Components
-                        <b class="caret"></b>
-                    </p>
-                </a>
-                <div class="collapse" id="componentsExamples">
-                    <ul class="nav">
-                        <li class="nav-item ">
-                            <a class="nav-link" data-toggle="collapse" href="#componentsCollapse">
-                                <span class="sidebar-mini"> MLT </span>
-                                <span class="sidebar-normal"> Multi Level Collapse
-                      <b class="caret"></b>
-                    </span>
-                            </a>
-                            <div class="collapse" id="componentsCollapse">
-                                <ul class="nav">
-                                    <li class="nav-item ">
-                                        <a class="nav-link" href="#">
-                                            <span class="sidebar-mini"> E </span>
-                                            <span class="sidebar-normal"> Example </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+            @foreach(config('mdp.menu', []) as $menuItem)
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ $menuItem->getLink() }}">
+                        {!! $menuItem->getIcon()->buildIcon() !!}
+                        <p> {{ $menuItem->getTitle() }} </p>
+                    </a>
+                </li>
+            @endforeach
+{{--            <li class="nav-item ">--}}
+{{--                <a class="nav-link" data-toggle="collapse" href="#pagesExamples">--}}
+{{--                    <i class="material-icons">image</i>--}}
+{{--                    <p> Pages--}}
+{{--                        <b class="caret"></b>--}}
+{{--                    </p>--}}
+{{--                </a>--}}
+{{--                <div class="collapse" id="pagesExamples">--}}
+{{--                    <ul class="nav">--}}
+{{--                        <li class="nav-item ">--}}
+{{--                            <a class="nav-link" href="#">--}}
+{{--                                <span class="sidebar-mini"> P </span>--}}
+{{--                                <span class="sidebar-normal"> Pricing </span>--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            </li>--}}
+{{--            <li class="nav-item ">--}}
+{{--                <a class="nav-link" data-toggle="collapse" href="#componentsExamples">--}}
+{{--                    <i class="material-icons">apps</i>--}}
+{{--                    <p> Components--}}
+{{--                        <b class="caret"></b>--}}
+{{--                    </p>--}}
+{{--                </a>--}}
+{{--                <div class="collapse" id="componentsExamples">--}}
+{{--                    <ul class="nav">--}}
+{{--                        <li class="nav-item ">--}}
+{{--                            <a class="nav-link" data-toggle="collapse" href="#componentsCollapse">--}}
+{{--                                <span class="sidebar-mini"> MLT </span>--}}
+{{--                                <span class="sidebar-normal"> Multi Level Collapse--}}
+{{--                      <b class="caret"></b>--}}
+{{--                    </span>--}}
+{{--                            </a>--}}
+{{--                            <div class="collapse" id="componentsCollapse">--}}
+{{--                                <ul class="nav">--}}
+{{--                                    <li class="nav-item ">--}}
+{{--                                        <a class="nav-link" href="#">--}}
+{{--                                            <span class="sidebar-mini"> E </span>--}}
+{{--                                            <span class="sidebar-normal"> Example </span>--}}
+{{--                                        </a>--}}
+{{--                                    </li>--}}
+{{--                                </ul>--}}
+{{--                            </div>--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            </li>--}}
         </ul>
     </div>
     <div class="sidebar-background"></div>

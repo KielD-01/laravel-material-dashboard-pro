513bf866b0b8bea83f845254cc907212e11d4153
Roman Kozin <romanko95.rk@gmail.com>
2021-06-07 13:18:39 +0300

    - Updates

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -42,8 +42,8 @@
 <script src="{{ asset('/assets/mdp/js/core/popper.min.js') }}"></script>
 <script src="{{ asset('/assets/mdp/js/core/bootstrap-material-design.min.js') }}"></script>
 <script src="{{ asset('/assets/mdp/js/plugins/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/material-dashboard.js') }}"></script>
 <script src="{{ asset('/assets/mdp/js/dom.js') }}"></script>
+<script src="{{ asset('/assets/mdp/js/material-dashboard.js') }}"></script>
 
 @stack('mdp::layout_js')
 

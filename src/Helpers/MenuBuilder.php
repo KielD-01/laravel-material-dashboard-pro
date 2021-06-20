d935b28de488d9e796f113415b3f5f449532a12d
Roman Kozin <romanko95.rk@gmail.com>
2021-06-20 23:58:09 +0300

    - Adde support for no icons and instatiatable icon classes

--- src/Helpers/MenuBuilder.php
+++ src/Helpers/MenuBuilder.php
@@ -5,34 +5,45 @@ declare(strict_types=1);
 namespace KielD01\LaravelMaterialDashboardPro\Helpers;
 
 use Illuminate\Support\Facades\Config;
+use KielD01\LaravelMaterialDashboardPro\Helpers\Icons\FontAwesomeIcon;
+use KielD01\LaravelMaterialDashboardPro\Helpers\Icons\MaterialIcon;
 
 class MenuBuilder
 {
-    public static function build(): array
-    {
-        return self::processMenu(
-            Config::get('mdp.menu', [])
-        );
-    }
+	public static function build(): array
+	{
+		return self::processMenu(
+			Config::get('mdp.menu', [])
+		);
+	}
 
-    private static function processMenu(array $menuItems, bool $isChild = false): array
-    {
-        foreach ($menuItems as $index => $menuItem) {
+	private static function processMenu(array $menuItems, bool $isChild = false): array
+	{
+		foreach ($menuItems as $index => $menuItem) {
+			$link = array_key_exists('link', $menuItem) ?
+				$menuItem['link'] :
+				['type' => MenuItemLinkType::URI, 'uri' => '#'];
 
-            $link = array_key_exists('link', $menuItem) ?
-                $menuItem['link'] :
-                ['type' => MenuItemLinkType::URI, 'uri' => '#'];
+			$hasIcon = array_key_exists('icon', $menuItem);
 
-            $menuItems[$index] = new MenuItem(
-                $menuItem['title'],
-                $link['type'],
-                $link[$link['type']],
-                $isChild ? null : $menuItem['icon'],
-                self::processMenu($menuItem['children'] ?? [], true),
-                $isChild
-            );
-        }
+			/** @var FontAwesomeIcon|MaterialIcon $iconClass */
+			$iconClass = null;
+			$icon = null;
 
-        return $menuItems;
-    }
+			if ($hasIcon) {
+				[$iconClass, $icon] = $menuItem['icon'];
+			}
+
+			$menuItems[$index] = new MenuItem(
+				$menuItem['title'],
+				$link['type'],
+				$link[$link['type']],
+				$isChild && !$hasIcon ? null : new $iconClass($icon),
+				self::processMenu($menuItem['children'] ?? [], true),
+				$isChild
+			);
+		}
+
+		return $menuItems;
+	}
 }
--- src/resources/views/layouts/user/auth-v1.blade.php
+++ src/resources/views/layouts/user/auth-v1.blade.php
@@ -36,7 +36,7 @@
 <!-- End Navbar -->
 <div class="wrapper wrapper-full-page">
     <div class="page-header login-page header-filter" filter-color="black"
-         style="background-image: url('{{ $mdp->getRandomBackgroundUrl() }}'); background-size: cover; background-position: top center;">
+         style="background-image: url('{{ $backgroundImage ?? $mdp->getRandomBackgroundUrl() }}'); background-size: cover; background-position: top center;">
         <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
         <div class="container">
             @yield('mdp::content')

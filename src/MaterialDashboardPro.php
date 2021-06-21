fafa8ffedeb5b92ff6109bf105479fba67c633b1
Roman Kozin <romanko95.rk@gmail.com>
2021-06-21 19:34:05 +0300

    - Added user set into mdp

--- src/MaterialDashboardPro.php
+++ src/MaterialDashboardPro.php
@@ -6,6 +6,7 @@ namespace KielD01\LaravelMaterialDashboardPro;
 
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Container\CircularDependencyException;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use KielD01\LaravelMaterialDashboardPro\Helpers\MenuBuilder;
 
@@ -14,11 +15,13 @@ class MaterialDashboardPro
 	private $request;
 	private $menu = [];
 	private $pageTitle = 'Page';
+	private $user = null;
 
 	public function __construct(Request $request)
 	{
 		$this->request = $request;
 		$this->setMenu();
+		$this->setUser($this->request->user());
 	}
 
 	public function getRandomBackgroundUrl(): string
@@ -35,9 +38,26 @@ class MaterialDashboardPro
 		$this->menu = resolve(MenuBuilder::class)->build();
 	}
 
+	/**
+	 * @param Model|null $user
+	 *
+	 * @return $this
+	 */
+	private function setUser(?Model $user): self
+	{
+		$this->user = $user;
+
+		return $this;
+	}
+
 	public function hasUser(string $guard = null)
 	{
 		return $this->request->user($guard) ?? false;
+	}
+
+	public function getUser()
+	{
+		return $this->user;
 	}
 
 	public function getPageTitle(): string

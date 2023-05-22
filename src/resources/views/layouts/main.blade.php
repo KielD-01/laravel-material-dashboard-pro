d4fab025d7ec5bf3273b931bf8b15e7dbcdd3a91
Roman <romanko95.rk@gmail.com>
2023-05-22 14:23:56 +0200

    Update main.blade.php

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -18,16 +18,16 @@
     @includeWhen(config('mdp.core.fixed_plugin.enabled', false), 'mdp::elements.fixed-plugin')
 </div>
 
-<script src="{{ asset('/assets/mdp/js/core/jquery.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/core/popper.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/core/bootstrap-material-design.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/core/bootstrap-selectpicker.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/plugins/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/dom.js') }}"></script>
+<script src="/assets/mdp/js/core/jquery.min.js"></script>
+<script src="/assets/mdp/js/core/popper.min.js"></script>
+<script src="/assets/mdp/js/core/bootstrap-material-design.min.js"></script>
+<script src="/assets/mdp/js/core/bootstrap-selectpicker.js"></script>
+<script src="/assets/mdp/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="/assets/mdp/js/dom.js"></script>
 
 @stack('mdp::before_material_js')
 
-<script src="{{ asset('/assets/mdp/js/material-dashboard.js') }}"></script>
+<script src="/assets/mdp/js/material-dashboard.js"></script>
 
 @stack('mdp::layout_js')
 

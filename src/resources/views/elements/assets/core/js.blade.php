e2a468513d5c2c662d97c55397775fff2f31fdbe
Roman <romanko95.rk@gmail.com>
2023-05-22 14:23:34 +0200

    Update js.blade.php

--- src/resources/views/elements/assets/core/js.blade.php
+++ src/resources/views/elements/assets/core/js.blade.php
@@ -1,9 +1,9 @@
-<script src="{{ asset('/assets/mdp/js/core/jquery.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/core/popper.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/core/bootstrap-material-design.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/plugins/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/dom.js') }}"></script>
-<script src="{{ asset('/assets/mdp/js/material-dashboard.js') }}"></script>
+<script src="/assets/mdp/js/core/jquery.min.js"></script>
+<script src="/assets/mdp/js/core/popper.min.js"></script>
+<script src="/assets/mdp/js/core/bootstrap-material-design.min.js"></script>
+<script src="/assets/mdp/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="/assets/mdp/js/dom.js"></script>
+<script src="/assets/mdp/js/material-dashboard.js"></script>
 
 @stack('mdp::layout_js')
 @yield('mdp::js')
